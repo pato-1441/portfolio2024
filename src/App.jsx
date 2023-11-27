128241d20875f1f6404888d22ee2fb21b1f9b04a
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-11-27 20:53:17 -0300

    nav ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
+import Nav from "./components/nav/Nav";
+
 function App() {
   return (
     <div>
       <div className="h-screen bg-black flex flex-col text-white">
-        <header className="w-full flex items-center justify-between p-4">
-          <div className="flex items-center gap-4">
-            <img
-              src="https://avatars.githubusercontent.com/u/83987715?v=4"
-              alt="duck shield logo"
-              className="w-8 rounded-full"
-            />
-            <h1 className="font-semibold">Patricio Albornoz</h1>
-          </div>
-          <a
-            className="transition-all duration-150 hover:scale-105 text-sm py-1 px-3 rounded-full border border-gray-200"
-            href="mailto:pwalbornoz@gmail.com"
-            rel="noreferrer noopener"
-          >
-            <span>pwalbornoz@gmail.com</span>
-          </a>
-        </header>
-
-        <main className="flex-1">
+        <Nav />
+        <main className="flex-1 bg-gray-900">
           <section className="text-center">
             <h2 className="text-3xl font-medium text-center">
               React.js Project Created using
             </h2>
+            <img
+              src="https://avatars.githubusercontent.com/u/83987715?v=4"
+              alt="patricio albornoz picture"
+              className="w-8 rounded-full"
+            />
           </section>
         </main>
       </div>
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,41 @@
+import { Github, SquareCode, Linkedin } from "lucide-react";
+
+const Nav = () => {
+  return (
+    <header className="w-full flex items-center justify-between p-4">
+      <div className="flex items-center gap-4">
+        <h1 className="font-semibold flex gap-2">
+          <span>
+            <SquareCode />
+          </span>
+          Patricio Albornoz
+        </h1>
+      </div>
+      <div className="flex items-center gap-4">
+        <a
+          className="transition-all duration-150 hover:scale-105 text-sm py-1 px-3 rounded-full border border-gray-200"
+          href="mailto:pwalbornoz@gmail.com"
+          rel="noreferrer noopener"
+        >
+          <span>pwalbornoz@gmail.com</span>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/pato-1441"
+        >
+          <Github size={22} className="hover:fill-white" />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/patoalbornoz/"
+        >
+          <Linkedin size={22} className="hover:fill-white" />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Nav;
