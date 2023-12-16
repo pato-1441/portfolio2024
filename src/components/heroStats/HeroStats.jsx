930a1a6da1cc31bbd6c9630e8bf1d192714b2451
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-12-15 22:10:53 -0300

    shadow component modified

--- src/components/heroStats/HeroStats.jsx
+++ src/components/heroStats/HeroStats.jsx
@@ -9,21 +9,21 @@ const HeroStats = () => {
     >
       <div
         className="transition-all duration-150 hover:shadow-white hover:shadow-component flex items-center sm:gap-8
-        justify-between w-full border rounded-l-xl px-5 py-4"
+        justify-between w-full border rounded-l-xl px-6 py-4"
       >
         <StatCard qty={"+3"} title={"Años\nprogramando"} />
         <TerminalSquare className="w-9 h-9" />
       </div>
       <div
         className="transition-all duration-150 hover:shadow-white hover:shadow-component flex items-center sm:gap-8
-        justify-between w-full border-t border-b px-5 py-4"
+        justify-between w-full border-t border-b px-6 py-4"
       >
         <StatCard qty={"+15"} title={"Tecnologías\nempleadas"} />
         <Braces className="w-9 h-9" />
       </div>
       <div
         className="transition-all duration-150 hover:shadow-white hover:shadow-component flex items-center sm:gap-8
-        justify-between w-full border rounded-r-xl px-5 py-4"
+        justify-between w-full border rounded-r-xl px-6 py-4"
       >
         <StatCard qty={"+10"} title={"Proyectos\nconcluidos"} />
         <BadgeCheck className="w-9 h-9" />
