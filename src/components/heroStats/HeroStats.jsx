85e770aad71f657e12c4a402875e6ce81f95e58b
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-12-15 22:19:51 -0300

    dark light mode

--- src/components/heroStats/HeroStats.jsx
+++ src/components/heroStats/HeroStats.jsx
@@ -8,22 +8,22 @@ const HeroStats = () => {
      h-fit "
     >
       <div
-        className="transition-all duration-150 hover:shadow-white hover:shadow-component flex items-center sm:gap-8
-        justify-between w-full border rounded-l-xl px-6 py-4"
+        className="text-black bg-gray-50 dark:bg-neutral-900 dark:text-white transition-all duration-150 hover:shadow-black dark:hover:shadow-white hover:shadow-component flex items-center sm:gap-8
+        justify-between w-full border rounded-xl rounded-b-none sm:border-r-none sm:rounded-tr-none sm:rounded-l-xl px-6 py-4"
       >
         <StatCard qty={"+3"} title={"Años\nprogramando"} />
         <TerminalSquare className="w-9 h-9" />
       </div>
       <div
-        className="transition-all duration-150 hover:shadow-white hover:shadow-component flex items-center sm:gap-8
-        justify-between w-full border-t border-b px-6 py-4"
+        className="text-black bg-gray-50 dark:bg-neutral-900 dark:text-white transition-all duration-150 hover:shadow-black dark:hover:shadow-white hover:shadow-component flex items-center sm:gap-8
+        justify-between w-full border-l border-r sm:border-l-none sm:border-r-none sm:border-t sm:border-b px-6 py-4"
       >
         <StatCard qty={"+15"} title={"Tecnologías\nempleadas"} />
         <Braces className="w-9 h-9" />
       </div>
       <div
-        className="transition-all duration-150 hover:shadow-white hover:shadow-component flex items-center sm:gap-8
-        justify-between w-full border rounded-r-xl px-6 py-4"
+        className="text-black bg-gray-50 dark:bg-neutral-900 dark:text-white transition-all duration-150 hover:shadow-black dark:hover:shadow-white hover:shadow-component flex items-center sm:gap-8
+        justify-between w-full border rounded-xl rounded-t-none sm:rounded-bl-none sm:rounded-r-xl px-6 py-4"
       >
         <StatCard qty={"+10"} title={"Proyectos\nconcluidos"} />
         <BadgeCheck className="w-9 h-9" />
